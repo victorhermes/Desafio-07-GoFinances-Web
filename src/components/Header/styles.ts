40ceb5e7d44bb0cafbd1b4b863531b508826c704
Victor Hermes <me.victorhermes@gmail.com>
2020-06-12 14:44:15 -0300

    navlink active

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -37,7 +37,8 @@ export const Container = styled.div<ContainerProps>`
         }
 
         &:hover {
-          opacity: 0.6;
+          border-bottom: 2px solid orange;
+          padding-bottom: 5px;
         }
       }
     }
